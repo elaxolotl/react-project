6acd624544f77d12137e37417c7435e4dfd4e580
youssefEchadySfaxi <140536366+elaxolotl@users.noreply.github.com>
2024-02-27 12:30:56 +0100

    W5 Arrow and handler Functions in React

--- myProject/src/App.jsx
+++ myProject/src/App.jsx
@@ -20,40 +20,40 @@ const list = [
 function getTitle(title) {
   return title;
 }
-function List(){
-  return (
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title} </a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-  );
-};
-function Search(){
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title} </a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+const Search = () => {
+  const handleChange=(event)=>{
+    console.log(event);
+    console.log(event.target.value);
+  }
   return (
     <div>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };
-function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle("React")}</h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
-  );
-}
+
+const App = () => (
+  <div>
+    <h1>Hello {getTitle("React")}</h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
+
 export default App;
